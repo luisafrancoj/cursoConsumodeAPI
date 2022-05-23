82e3c61f539fdda8cebd30bf1af7da7e4c3ae98c
Luisa Franco <lmfj95@gmail.com>
2022-05-23 16:19:25 -0500

    Working APIS with callback

---
+++ src/callback/index.js
@@ -0,0 +1,31 @@
+function sum(num1, num2){
+  return num1 + num2;
+};
+
+function rest(num1, num2){
+  return num1 - num2;
+};
+
+function calc(num1, num2, callback){
+  return callback(num1, num2)
+};
+
+console.log(calc(6,2,sum));
+console.log(calc(6,2,rest));
+
+function date(luisa) {
+  console.log(new Date);
+  setTimeout (function (){
+    let date =  new Date;
+    luisa(date);
+  },3000)
+}
+
+function printDate(dateNow){
+  console.log(dateNow)
+}
+
+
+date(printDate);
+
+
